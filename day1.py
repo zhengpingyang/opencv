93deb77d8a4379d9411074633f896f716789cc8e
zhengpingyang <963979034@qq.com>
2020-08-24 11:06:01 +0800

    day1

---
+++ day1.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+# author:zpy time:2020/8/1  11:34
+# file:day1.py
+# software:{PyCharm}
+# start = datetime.datetime.now()
+# end = datetime.datetime.now()
+# print('final is in:%s seconds' % (end - start))
+
+
+# read & show image
+import cv2 as cv
+
+
+img = cv.imread('image.jpg')
+cv.namedWindow('img', cv.WINDOW_AUTOSIZE)
+cv.imshow('img', img)
+cv.imwrite('image1.jpg',img)
+
+cv.waitKey()
+cv.destroyAllWindows()
